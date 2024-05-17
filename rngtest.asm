aa1d48b619bc6accc160e2d37b63a23e807d0ba6
Narwhail <john.m.ocampo@gmail.com>
2024-05-18 04:41:48 +0800

    test

---
+++ rngtest.asm
@@ -0,0 +1,40 @@
+.model small
+.stack 100h
+.data
+    outputMsg db "Random Number between (0-3): $"
+    randomNum db 0
+.code
+main proc far
+
+    mov ax, @data
+    mov ds, ax
+
+    call rng
+
+    mov ah, 09h
+    mov dx, offset outputMsg
+    int 21h
+
+    mov ah, 02h
+    mov dl, randomNum
+    add dl, '0'
+    int 21h
+
+    mov ah, 4ch
+    int 21h
+main endp
+
+rng proc near
+    mov ah, 00h
+    int 1ah
+
+    mov ax, dx
+    mov dx, 00h
+    mov bx, 04h
+    div bx
+
+    mov randomNum, dl
+    ret
+rng endp
+
+end main
