bdb290414d5ff0d9aaaed2e7529ffdd3624506bd
Narwhail <john.m.ocampo@gmail.com>
2024-05-17 19:37:57 +0800

    Added single obstacle and collission check + additional comments

--- game.asm
+++ game.asm
@@ -1,24 +1,47 @@
 .model small
 .stack
 .data
+    ;strings
+    ;playing state
     line1_game db "MAHARLIKA", "$"
     line2_game db "ASCENDANCE", "$"
     line3_game db "Control using", "$"
     line4_game db "'WASD'", "$"
     line5_game db "Score:" ,"$"
+    ;gameover state
+    line1_over db "GAME OVER", "$"
+    line2_over db "Score: ", "$"
+    line3_over db "Press any key to return to menu", "$"
+
+
+    
+    
+    ;game variables
     current_tick db 00h
+    y_toplimit dw 18h
+    y_bottomlimit dw 098h
+    y_velocity dw 0004h         ;this is used for obstacles, and tower y movement
+    game_state dw 0001h         ;0 = title screen, 1 = playing, 2 = game over
+    
+    ;character variables
     char_size dw 0fh
     char_x dw 0088h
     char_y dw 0098h
     char_velocity dw 0008h
-    char_xfixedpos dw 01h
-    y_toplimit dw 18h
-    y_bottomlimit dw 098h
+    char_xfixedpos dw 01h       ;0087h, 00b7h, 00dfh, 010fh
+
+    ;tower
     tower_x dw 0097h
     tower_x2 dw 0efh
     tower_y dw 0008h
     tower_xsize dw 1fh
     tower_ysize dw 0afh
+
+    ;obstacle       fixed positions = 0087h, 00b7h, 00dfh, 010fh
+    obstacle_xpos dw 00b7h
+    obstacle_ypos dw 0008h
+    
+    
 
 .code
 
@@ -30,12 +53,12 @@ org 0100h
 
         call clear_screen
 
-        check_tick:
+        playing_game:
             mov ah, 2ch
             int 21h
 
             cmp dl, current_tick
-            je check_tick
+            je playing_game
 
             mov current_tick, dl        ;update game tick
             
@@ -50,13 +73,155 @@ org 0100h
             mov bx, tower_x2
             call draw_tower
 
+            
+
+            call draw_obstacle
+            call move_obstacle
             call draw_char
-            call game_printtext
-            ;call game_printscore
-            call game_input
-            jmp check_tick
+            call playinggame_printtext
+            ;call playinggame_printscore
+            call playinggame_input
+            call check_collission
+
+            cmp game_state, 01h         ;if game state is still playing, continue to next tick
+            je playing_game             ;if true
+            jmp game_over               ;else
+
+        game_over:
+            call gameover_printtext     
+            call gameover_input         ;check for input
     main endp
 
+    default_gamevalue proc near
+        mov char_xfixedpos, 01h
+        mov char_y, 0098h
+        ret
+    default_gamevalue endp 
+
+    gameover_input proc near
+        mov ah, 01h
+        int 16h
+        jnz gameover_keypress       ;if zero flag is false, go to _keypress        
+        jmp gameover_input          ;else keep checking for input
+
+        gameover_keypress:
+            mov game_state, 01h
+            call default_gamevalue
+            jmp playing_game
+    gameover_input endp
+
+    gameover_printtext proc near
+        call clear_screen
+
+        mov ah, 02h
+        mov bh, 00h         ;page position
+        mov dh, 04h         ;y position of text -> 1 hexadecimal is equivalent to 8 pixels
+        mov dl, 04h         ;x position of text -> 00h is tile one, 01h is tile two
+        int 10h
+        mov ah, 09h
+        mov dx, offset line1_over       ;smd
+        int 21h
+
+        mov ah, 02h
+        mov bh, 00h         ;page position
+        mov dh, 07h         ;y position of text -> 1 hexadecimal is equivalent to 8 pixels
+        mov dl, 04h         ;x position of text -> 00h is tile one, 01h is tile two
+        int 10h
+        mov ah, 09h
+        mov dx, offset line2_over
+        int 21h
+
+        mov ah, 02h
+        mov bh, 00h         ;page position
+        mov dh, 09h         ;y position of text
+        mov dl, 04h         ;x position of text -> 00h is tile one, 01h is tile two
+        int 10h
+        mov ah, 09h
+        mov dx, offset line3_over
+        int 21h
+        ret
+    gameover_printtext endp
+
+    move_obstacle proc near
+        mov ax, y_velocity  
+        add obstacle_ypos, ax           ;obstacle_ypos += y_velocity
+
+        mov ax, y_bottomlimit
+        add ax, 10h                     ;add bottom limit by 16 pixels
+
+        cmp obstacle_ypos, ax           ;compare it to bottom limit
+        jl exit_moveobs                 ;if its less
+
+        ;else
+        mov obstacle_ypos, 0008h        ;reset back to top
+        ret
+
+        exit_moveobs:
+            ret
+    move_obstacle endp
+
+    check_collission proc near
+        ;check if char is colliding with obstacle
+        ;char_x+char_size > obstacle_xpos && char_x < obstacle_xpos+char_size 
+        ;&& char_y+char_size > obstacle_ypos && char_y < obstacle_ypos+char_size 
+
+        mov ax, char_x
+        add ax, char_size
+        cmp ax, obstacle_xpos
+        jng exit_collission
+
+        mov ax, obstacle_xpos
+        add ax, char_size
+        cmp char_x, ax
+        jnl exit_collission
+
+        mov ax, char_y
+        add ax, char_size
+        cmp ax, obstacle_ypos
+        jng exit_collission
+
+        mov ax, obstacle_ypos
+        add ax, char_size
+        cmp char_y, ax
+        jnl exit_collission
+
+        ;if collission is true
+        mov game_state, 02h         ;set game state to gameover
+        ret
+
+        ;if false
+        exit_collission:
+            ret
+
+    check_collission endp
+
+    draw_obstacle proc near
+
+        ;single obstacle
+        mov cx, obstacle_xpos     ;x position is also fixed
+        mov dx, obstacle_ypos     ;y position is same as 
+
+        drawobs:
+            mov ah, 0ch     ;set config to write pixels
+            mov al, 0fh     ;set pixel color to white
+            mov bh, 00h     ;page number
+            
+            int 10h         ;execute
+
+            inc cx
+            mov ax, cx
+            sub ax, obstacle_xpos
+            cmp ax, char_size
+            jng drawobs
+            mov cx, obstacle_xpos
+            inc dx
+            mov ax, dx 
+            sub ax, obstacle_ypos
+            cmp ax, char_size
+            jng drawobs
+        ret
+    draw_obstacle endp
+    
     draw_tower proc near
         mov cx, bx
         mov dx, tower_y     ;y position
@@ -80,8 +245,33 @@ org 0100h
             jng tower_loop
         ret
     draw_tower endp
+    
+    draw_char proc near
+        mov cx, char_x     ;x position
+        mov dx, char_y     ;y position
 
-    game_printtext proc near
+        drawchar_horizontal:
+            mov ah, 0ch     ;set config to write pixels
+            mov al, 31h     ;set pixel color to green
+            mov bh, 00h     ;page number
+            
+            int 10h         ;execute
+
+            inc cx
+            mov ax, cx
+            sub ax, char_x
+            cmp ax, char_size
+            jng drawchar_horizontal
+            mov cx, char_x
+            inc dx
+            mov ax, dx 
+            sub ax, char_y
+            cmp ax, char_size
+            jng drawchar_horizontal
+        ret
+    draw_char endp
+
+    playinggame_printtext proc near
         mov ah, 02h
         mov bh, 00h         ;page position
         mov dh, 04h         ;y position of text -> 1 hexadecimal is equivalent to 8 pixels
@@ -126,39 +316,19 @@ org 0100h
         mov ah, 09h
         mov dx, offset line5_game       ;score
         int 21h
-    game_printtext endp
+        ret
+    playinggame_printtext endp
 
-    game_printscore proc near
+    playinggame_printscore proc near           ;incomplete
         
         ret
-    game_printscore endp
+    playinggame_printscore endp
+           
+    playinggame_input proc near
+        mov ah, 01h             ;if no key is pressed, exit playinggame_input
+        int 16h
+        jz exit_input
 
-    draw_char proc near
-        mov cx, char_x     ;x position
-        mov dx, char_y     ;y position
-
-        drawchar_horizontal:
-            mov ah, 0ch     ;set config to write pixels
-            mov al, 31h     ;set pixel color to green
-            mov bh, 00h     ;page number
-            
-            int 10h         ;execute
-
-            inc cx
-            mov ax, cx
-            sub ax, char_x
-            cmp ax, char_size
-            jng drawchar_horizontal
-            mov cx, char_x
-            inc dx
-            mov ax, dx 
-            sub ax, char_y
-            cmp ax, char_size
-            jng drawchar_horizontal
-        ret
-    draw_char endp
-        
-    game_input proc near
         mov ah, 00h
         int 16h
 
@@ -228,7 +398,7 @@ org 0100h
             jmp exit_input
         exit_input:
             ret
-    game_input endp
+    playinggame_input endp
 
     update_xposition proc near      ;this is responsible for updating the values for adding in char_x, depending on the char_xfixedpos
         mov ax, char_xfixedpos
